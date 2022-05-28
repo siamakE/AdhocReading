75edd714585f1c2d5ed25aeb12d34427812ba2be
Siamak Esmaeili <siamak@abacus.co>
2022-05-28 11:58:50 +0430

    MarkJPrice Book

---
+++ AdhocReading/SpeakingCSharp.cs
@@ -0,0 +1,48 @@
+﻿namespace AdhocReading
+{
+    [TestClass]
+    public class SpeakingCSharp
+    {
+        [TestMethod]
+        public void VariableName()
+        {
+            var todo = 1;
+            Assert.AreEqual("todo",nameof(todo));
+        }
+        [TestMethod]
+        public void CompareDouble()
+        {
+            var double1 = 0.1;
+            var double2 = 0.2;
+            Assert.IsFalse(double1 + double2 == 0.3);
+
+            var decimal1 = 0.1M;
+            var decimal2 = 0.2M;
+            Assert.IsTrue(decimal1 + decimal2 == 0.3M);
+        }
+        [TestMethod]
+        public void PatternMatching1()
+        {
+            object o1 = "3";
+            Assert.IsFalse(o1 is int);
+
+            object o2 = 3;
+            Assert.IsTrue(o2 is int);
+        }
+        private int FibFunctional(int n) => n switch
+        {
+            1 => 0,
+            2 => 1,
+            _ => FibFunctional(n - 2) + FibFunctional(n - 1),
+        };
+        [TestMethod]
+        public void FibTest()
+        {
+            Assert.AreEqual(0, FibFunctional(1));
+            Assert.AreEqual(1, FibFunctional(2));
+            Assert.AreEqual(1, FibFunctional(3));
+            Assert.AreEqual(2, FibFunctional(4));
+            Assert.AreEqual(3, FibFunctional(5));
+        }
+    }
+}
---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace PacktLibrary;
+
+public class Person
+{
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public List<Person> Children = new ();
+    public static decimal InterestRate;
+}
+public class TextAndNumber
+{
+    public string Text { get; set; }
+    public decimal Number { get; set; }
+}
+public class LifeTheUniverseAndEverything
+{
+    public TextAndNumber GetTheData() => new TextAndNumber
+    {
+        Text = "What's the meaning of life?",
+        Number = 42
+    };
+    public (string Text, int Number) GetTheDataTuple() => (Text: "What's the meaning of life?", Number: 42);
+    
+}
+
+public record ImmutableVehicle
+{
+    public int Wheels { get; init; }
+    public string Color { get; init; }
+    public string Brand { get; init; }
+}
