1432e6e9bf9b7461b134b9acb81ceffcaf8451ea
Siamak Esmaeili <siamak@abacus.co>
2022-06-06 09:16:34 +0430

    some sql stuff

---
+++ AdhocReading/DatabaseTests.cs
@@ -0,0 +1,14 @@
+﻿using WorkingWithEFCore;
+
+namespace AdhocReading
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void DatabaseProvider()
+        {
+            Assert.AreEqual("SQLServer", ProjectConstants.DatabaseProvider);
+        }
+    }
+}
---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PacktLibrary;
+
+namespace WorkingWithEFCore
+{
+    public class Northwind : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=.;Initial Catalog=Northwind;Integrated Security=true;MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            // example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired() // NOT NULL
+            .HasMaxLength(15);
+        }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
